ff02aaacb0b9cb3df950552168ddeffc78bc731a
Adam Bassett <acbassettone@gmail.com>
2019-07-10 01:38:51 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -1,18 +1,6 @@
-var grilledCheeseIngredients = [
-  'bread',
-  'mild cheese',
-  'sharp cheese',
-  'butter',
-  'tomato',
-  'garlic'
-]
- 
-var tomatoSauceIngredients = [
-  'tomato',
-  'garlic',
-  'olive oil',
-  'basil',
-  'oregano'
-]
-
-var myArray = [element0, element1, elementN];
+var chocolateBars = [
+  'snickers',
+  'hundred grand',
+  'kitkat',
+  'skittles',
+  ]
